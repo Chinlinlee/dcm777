f86f1b7ef7df045a0f338b33e4b90cc2d91a6ef8
chin <a5566qq2581@gmail.com>
2023-08-20 18:15:08 +0800

    feat: add basic function for StudyQueryTask
    - wrappedFindNextStudy

--- src/main/java/org/github/chinlinlee/dcm777/net/StudyQueryTask.java
+++ src/main/java/org/github/chinlinlee/dcm777/net/StudyQueryTask.java
@@ -1,7 +1,9 @@
 package org.github.chinlinlee.dcm777.net;
 
+import java.io.IOException;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.net.Association;
+import org.dcm4che3.net.Status;
 import org.dcm4che3.net.pdu.PresentationContext;
 import org.dcm4che3.net.service.DicomServiceException;
 
@@ -9,10 +11,19 @@ public class StudyQueryTask extends PatientQueryTask {
 
     StudyQueryTaskInject myStudyQueryTaskInject;
 
-    StudyQueryTask(Association as, PresentationContext pc, Attributes rq, Attributes keys,
+    public StudyQueryTask(Association as, PresentationContext pc, Attributes rq, Attributes keys,
             QueryTaskInject queryTaskInject, PatientQueryTaskInject patientQueryTaskInject,
             StudyQueryTaskInject studyQueryTaskInject) throws DicomServiceException {
         super(as, pc, rq, keys, queryTaskInject, patientQueryTaskInject);
         myStudyQueryTaskInject = studyQueryTaskInject;
+        wrappedFindNextStudy();
+    }
+
+    private void wrappedFindNextStudy() throws DicomServiceException {
+        try {
+            this.myStudyQueryTaskInject.findNextStudy();
+        } catch (IOException e) {
+            throw new DicomServiceException(Status.UnableToProcess, e);
+        }
     }
 }
