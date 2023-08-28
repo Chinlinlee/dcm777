cf27ecd097cd75e28cd373fd313c054e2e6e0428
chin <a5566qq2581@gmail.com>
2023-08-28 15:43:50 +0800

    feat: convert audit message to json string

--- src/main/java/org/github/chinlinlee/dcm777/common/Common.java
+++ src/main/java/org/github/chinlinlee/dcm777/common/Common.java
@@ -2,13 +2,18 @@ package org.github.chinlinlee.dcm777.common;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import javax.json.Json;
+import org.dcm4che3.audit.AuditMessage;
+import org.dcm4che3.audit.AuditMessages;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.json.JSONReader;
 import org.dcm4che3.util.SafeClose;
+import org.json.JSONObject;
+import org.json.XML;
 import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
@@ -45,5 +50,12 @@ public class Common {
             // StatusPrinter will handle this
         }
         StatusPrinter.printInCaseOfErrorsOrWarnings(context);
+    }
+
+    public static String convertAuditMessageToJsonString(AuditMessage message) throws IOException {
+        String xmlString = AuditMessages.toXML(message);
+        JSONObject jsonObj = XML.toJSONObject(xmlString);
+
+        return jsonObj.toString();
     }
 }
